660ba9a1bc577d6c784128e67cc9eeaa487a65b4
Franck Cai <senlouboi.egame@gmail.com>
2025-04-02 18:40:40 +0200

    Qr code genertor done styling pending

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useRef } from "react"
+import { QRCodeCanvas } from "qrcode.react"
+import "./styles.css"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = useState<string>("")
+  const qrRef = useRef<HTMLCanvasElement>(null);
+  function handleSubmit(event : React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    setText("")
+    handleDownload()
+  }
+
+  const handleDownload = () => {
+    if (qrRef.current) {
+      const canvas = qrRef.current;
+      const url = canvas.toDataURL("image/png");
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = `${text}-code.png`;
+      link.click();
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='w-full h-screen flex flex-col'>
+      <div className="mx-auto flex gap-5 items-center">
+        <h1 className='text-4xl text-sky-700  font-bold'>QR Snap</h1>
+        <picture>
+          <img className="max-w-50 h-auto" src="/qr-snap-logo.png" alt="Qr Snap logo" />
+        </picture>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="max-w-sm">
+        <p>Generate. Scan. Share</p>
+        <form className="flex flex-col" onSubmit={(e) =>{handleSubmit(e)}}>
+          <input
+          required
+            type="text"
+            placeholder="Enter text to generate QR"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            className="p-2 mb-4 rounded text-slate-800"
+          />
+          <button className="bg-slate-800 px-4 py-1 text-slate-100 rounded cursor-pointer">Download QR Code</button>
+        </form>
+        {text && 
+          <div className="bg-white p-4 rounded w-sm"> 
+            <QRCodeCanvas ref={qrRef} value={text} size={1024}
+            bgColor="#ffffff" // Background color for padding
+              fgColor="#000000" // Foreground color for the QR code
+            />
+          </div>
+        }
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      
+      
+    </div>
   )
 }
 
